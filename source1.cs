14fc55d774825c35caa9c9dce5fa43146645d8c4
Adriano Carlos Verona <adriano@unity3d.com>
2024-11-28 08:25:36 -0300

    mais um

--- source1.cs
+++ source1.cs
@@ -3,3 +3,20 @@ void Foo() {}
 
 [Test(Ignore="Depends on issue #194 to work")]
 void Bar() {}
+
+[Test(Name="Foo", Ignore="This should match #194")]
+
+
+Test(
+Ignore="Depends on issue #194 to work")
+This is a fale positive..
+
+[Test(IgnoreNao="Another falsde positive for #194")]
+
+
+[Ignore("bla bla #194 bla bla")]
+void FooBar() {}
+
+void FooBar() {
+    var x = IgnoreReason="do not works.. #194 bla bla";
+}
