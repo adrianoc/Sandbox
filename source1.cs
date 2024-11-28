1c35cf05d76ece41857334d36e37618fcb5996a8
Adriano Carlos Verona <adriano@unity3d.com>
2024-11-28 08:38:58 -0300

    mais um

--- source1.cs
+++ source1.cs
@@ -1,22 +1,22 @@
-[Test(Ignore="Depends on issue #194 to work")]
+[Test(Ignore="Depends on issue #193 to work")]
 void Foo() {}
 
-[Test(Ignore="Depends on issue #194 to work")]
+[Test(Ignore="Depends on issue #193 to work")]
 void Bar() {}
 
-[Test(Name="Foo", Ignore="This should match #194")]
+[Test(Name="Foo", Ignore="This should match #193")]
 
 
 Test(
-Ignore="Depends on issue #194 to work")
+Ignore="Depends on issue #193 to work")
 This is a fale positive..
 
-[Test(IgnoreNao="Another falsde positive for #194")]
+[Test(IgnoreNao="Another falsde positive for #193")]
 
 
-[Ignore("bla bla #194 bla bla")]
+[Ignore("bla bla #193 bla bla")]
 void FooBar() {}
 
 void FooBar() {
-    var x = IgnoreReason="do not works.. #194 bla bla";
+    var x = IgnoreReason="do not works.. #193 bla bla"; 
 }
